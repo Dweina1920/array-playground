594e2a695a10bae36f427b79e16370a72970a419
Dina <dina.yamanid@gmail.com>
2023-05-04 13:55:47 +0200

    to ex 6 done

--- ex5.js
+++ ex5.js
@@ -4,9 +4,11 @@ const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present',
 
 // Usa el método filter para filtrar las palabras que empiezan por la letra 'e'
 
-console.log(words.filter()); // TODO [ 'elite', 'exuberant']
+console.log(words.filter(word => word[0] == "e")); // TODO [ 'elite', 'exuberant']
 
 // Usa el método filter para filtrar las palabras que empiezan por vocal
 // BONUS: Usa una expresión regular!
 
-console.log(words.filter()); // TODO [ 'elite', 'exuberant', 'away']
+const vocales = ["a", "e", "i", "o", "u"];
+
+console.log(words.filter(word => vocales.includes(word[0]))); // TODO [ 'elite', 'exuberant', 'away']
